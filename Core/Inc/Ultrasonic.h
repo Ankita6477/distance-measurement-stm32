a4e24efb27f7ad521860b30df4156427e3364e20
Ankita6477 <ankitabirajdar64@gmail.com>
2025-09-13 22:57:52 +0530

    Create Ultrasonic.h

---
+++ Core/Inc/Ultrasonic.h
@@ -0,0 +1,17 @@
+#ifndef INC_ULTRASONIC_H_
+#define INC_ULTRASONIC_H_
+
+#include "main.h"
+
+// Configure your pins here or in CubeMX main.h
+#define TRIG_PIN GPIO_PIN_9
+#define TRIG_PORT GPIOA
+#define ECHO_PIN GPIO_PIN_8
+#define ECHO_PORT GPIOA
+
+// Functions
+void Ultrasonic_Init(void);
+float Ultrasonic_Read(void);
+
+
+#endif /* INC_ULTRASONIC_H_ */
